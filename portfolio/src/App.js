aba67377fbe463bd8b373362fabad92fb6d7a714
zach-morris-text <oneblondhuman@gmail.com>
2024-11-18 21:36:34 -0500

    Construct UI skeleton for code-along visual

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import DevCol from './components/developer-column';
+import ProjCol from './components/projects-column';
+
 
 function App() {
   return (
     <div className="App">
-
+      <ProjCol />
+      <DevCol />
     </div>
   );
 }
---
+++ portfolio/src/components/projects-column.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+
+function ProjCol() {
+  return (
+    <div className="projCol">
+        <h3>Freelance</h3>
+        <h3>Personal</h3>
+        <h3>Planned</h3>
+        <h3>Study</h3>
+    </div>
+  );
+}
+
+export default ProjCol;
